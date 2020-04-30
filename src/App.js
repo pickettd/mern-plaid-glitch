efcbdf33f60e250aafafa428e32dad05d6274f22
David Pickett <pickettd@gmail.com>
2020-04-30 11:23:42 -1000

    Enable sass

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
 
-import "./App.css";
+import "./App.scss";
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
